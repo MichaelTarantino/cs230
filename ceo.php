434ed22d4d7fa6eccf7d7a99ba179d123217a0c3
MichaelTarantino <mtt0011@mix.wvu.edu>
2021-02-02 23:36:43 -0500

    Lab2 About/CEO page

---
+++ ceo.php
@@ -0,0 +1,43 @@
+<main>
+    <link rel="stylesheet" href="styles/about.css">
+    <title>About Monster Review's CEO</title>
+
+    <div class="about">
+        <div class="buttonBar">
+            <a class="button" href="index.php">Home</a>
+            <a class="button" style="left: 75px" href="ceo.php">CEO</a>
+        </div>
+        <h1 class="whiteText">CEO</h1>
+        <p>Chief Executive Officer</p>
+    </div>
+
+    <div class="CEOMainCol">
+        <div class="ceoColF"><br></div>
+        <div class="ceoColL">
+           <img class="bigImg" src="images/CEO.jpg">
+        </div>
+        <div class="ceoColM">
+            <p class="ceoName">Michael T. Tarantino</p>
+            <p class="ceoTitle">Chief Executive Officer</p>
+            <hr>
+            <div class="descCol">
+                <h3 style="margin-bottom: 0;"><b>PHONE</b></h3>
+                <p class="whiteText" style="margin-top: 4; margin-bottom: 20; font-size: large;">123-456-7890</p>
+            </div>
+            <div class="descCol">
+                <h3 style="margin-bottom: 0;"><b>EMAIL</b></h3>
+                <p class="whiteText" style="margin-top: 4; margin-bottom: 20; font-size: large;">mTarantino@anEmail.com</p>
+            </div>
+            <div class="CEOMainCol">
+                <h2 class="bioH">Biography</h2>
+                <p class="bioP"> &emsp;Hello, my name is Michael Tarantino and I am the proud owner and founder of Big Monster Reviews. As a team here at BMR
+                    we try to inspire individuals to make educated and well informed decisions on which Monster flavor they should purchase. With our 
+                    pationate team, we believe that we can be the ones to bring factual information to you, so that you can make amazing decisions on
+                    which monster flavors you should be purchasing and drinking. With that being said, I invite you to explore this wonderful website 
+                    and discover amazing new monster flavors that may be interesting to you!
+                </p>
+            </div>
+        </div>
+        <div class="ceoColR"><br></div>
+    </div>
+</main>
---
+++ index.php
@@ -0,0 +1,60 @@
+<main>
+    <link rel="stylesheet" href="styles/about.css">
+    <title>The spot for Hot Monster Reviews!</title>
+
+    <div class="about">
+        <div class="buttonBar">
+            <a class="button" href="index.php">Home</a>
+            <a class="button" style="left: 75px" href="ceo.php">CEO</a>
+        </div>
+        <h1 class="whiteText">About Us</h1>
+        <p>Big Monster Reviews</p>
+    </div>
+    <div class="row">
+
+        <div class="column">
+            <div class="card">
+                <img src="images/monsterReviewer.jpg">
+                <div class="container">
+                    <h2 class="whiteText">Head Reviewer</h2>
+                    <p>Robert Downey Jr</p>
+                    <p>Email: ironMan@anEmail.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/headOfStaff.jpg">
+                <div class="container">
+                    <h2 class="whiteText">Chief of Staff</h2>
+                    <p>Dwayne Johnson</p>
+                    <p>Email: theRock@anEmail.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/employeeOfMonth.jpg">
+                <div class="container">
+                    <h2 class="whiteText">Employee Of the Month</h2>
+                    <p>Curious George</p>
+                    <p>Email: curiousGeorge@anEmail.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/communications.jpg">
+                <div class="container">
+                    <h2 class="whiteText">Customer Service</h2>
+                    <p>Captain Jack Sparrow</p>
+                    <p>Email: theGreatestPirate@anEmail.com</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</main>
