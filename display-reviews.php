2e0d70825b608818785ad5066f84e02af2b17883
MichaelTarantino <mtt0011@mix.wvu.edu>
2021-03-25 15:04:52 -0400

    dispReviews

--- display-reviews.php
+++ display-reviews.php
@@ -12,3 +12,33 @@ if (!$conn) {
     # code...
 }
 
+$item = $_GET['id'];
+
+$sql= "SELECT * FROM reviews where itemid='$item'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        $uname = $row['uname'];
+        $profpic = "SELECT profilepic FROM profiles WHERE username = '$uname';";
+        $res = mysqli_query($conn, $profpic);
+        $picpath = mysqli_fetch_assoc($res);
+
+        echo'    
+        <div class="card mx-auto" style="width: 30%; padding: 5px; margin-bottom: 10px;">
+            <div class="media">
+                <img class="mr-3" src="'.$picpath['profilepic'].'" style="width: 75px; min-height: 75px; max-height: auto; border-radius: 50%;">
+                <div class="media-body">
+                    <h5 class="mt-0">'.$row['uname'].'</h5>
+                    <h5>'.$row['ratingnum'].'/5</h5>
+                    <p>'.$row['revdate'].'</p>
+                    <p>'.$row['reviewtext'].'</p>
+                </div>
+            </div>
+        </div>
+        ';
+    }
+}
+else{
+    echo'<h5 style="text-align: center;">No reviews, yet. Be the first to review!</h5>';
+}
--- includes/get-ratings.php
+++ includes/get-ratings.php
@@ -2,7 +2,26 @@
 
 include 'dbhandler.php';
 
+$id = $_GET['id'];
+$sqlAvg = "SELECT AVG(ratingnum) AS AVGRATE FROM reviews WHERE itemid='$id';";
+$sqlCount = "SELECT count(ratingnum) AS total FROM reviews WHERE itemid='$id';";
 
+$queryAvg = mysqli_query($conn, $sqlAvg);
+$queryCount = mysqli_query($conn, $sqlCount);
+
+$rowA = mysqli_fetch_array($queryAvg);
+$rowC = mysqli_fetch_array($queryCount);
+
+$avg = round($rowA['AVGRATE'], 1);
+$count = $rowC['total'];
+
+echo'
+    <div class="container" style="text-align: center;">
+        <h1>'.$avg.'</h1>
+        <div class="container">'.stars($avg).'</div>
+        <p>Number of Ratings: '.$count.'</p>
+    </div>
+';
 
 function stars($av){
     $s = "";
@@ -12,10 +31,10 @@ function stars($av){
         }  
     }
     for ($i=0; $i < floor($av); $i++) { 
-        $s .= '<i class="fa fa-star fa-2x" style="color:goldenrod"></i>';
+        $s .= '<i class="fa fa-star fa-2x" style="color:rgb(149, 214, 0)"></i>';
     }
     if (($av - floor($av)) > 0.4) {
-        $s .= '<i class="fas fa-star-half fa-2x" style="color:goldenrod"></i>';
+        $s .= '<i class="fas fa-star-half fa-2x" style="color:rgb(149, 214, 0)"></i>';
     }
     return $s;
 }
